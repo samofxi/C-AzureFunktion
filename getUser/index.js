9c82753fc273c4867d3711ea77bdeed27d302cdf
Salman Mohammad <sm@iaag.net>
2022-07-04 10:00:29 +0200

    Update Index.js

--- getUser/index.js
+++ getUser/index.js
@@ -1,11 +1,8 @@
-require('dotenv').config();
-const service = require('../bin/Services');
-require('dotenv').config();
 module.exports = async function (context, req) {
-    const results = await service.Home(JSON);
-    const responseMessage = "Hello, " + ". This HTTP triggered function executed successfully."
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  const responseMessage =
+    "Hello, This HTTP triggered function executed successfully.";
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
+};
